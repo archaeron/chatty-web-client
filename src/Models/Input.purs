97efd93b2fbed556b4c0ef700ed7d4219fc025ca
archaeron <whatamaps@gmail.com>
2015-03-04 13:59:27 +0100

    add an Eq instance for the InputType

--- src/Models/Input.purs
+++ src/Models/Input.purs
@@ -5,3 +5,11 @@ data InputType
 	| CodeInput
 	| FormulaInput
 	| FileInput
+
+instance eqInputType :: Eq InputType where
+	(==) TextInput TextInput = true
+	(==) CodeInput CodeInput = true
+	(==) FormulaInput FormulaInput = true
+	(==) FileInput FileInput = true
+	(==) _ _ = false
+	(/=) a b = not $ a == b
